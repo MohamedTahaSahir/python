5fe24ca34c36ae420f465f0ee62adc02d0579ead
MohamedTahaSahir <159806448+MohamedTahaSahir@users.noreply.github.com>
2024-02-14 13:01:32 +0100

    Update exercice.4.py

--- PHYTON/exercice.4.py
+++ PHYTON/exercice.4.py
@@ -1,18 +1,9 @@
 
-
 import sys
 
 
 def count_characters(text):
-    """
-    Count the number of uppercase letters, lowercase letters, punctuation marks, digits, and spaces in the text.
-
-    Args:
-        text (str): The input text.
-
-    Returns:
-        tuple: A tuple containing the counts of uppercase letters, lowercase letters, punctuation marks, digits, and spaces.
-    """
+    
     upper_count = sum(1 for char in text if char.isupper())
     lower_count = sum(1 for char in text if char.islower())
     punctuation_count = sum(1 for char in text if char in "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
@@ -23,12 +14,7 @@ def count_characters(text):
 
 
 def main():
-    """
-    Main function to execute the program.
-
-    Returns:
-        None
-    """
+    
     if len(sys.argv) == 1:
         text = input("What is the text to count?\n")
     elif len(sys.argv) == 2:
@@ -53,3 +39,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
