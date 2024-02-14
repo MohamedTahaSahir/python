216eaef0ff037c8c092fdb80fbb9a2a645804a73
MohamedTahaSahir <159806448+MohamedTahaSahir@users.noreply.github.com>
2024-02-14 13:18:07 +0100

    Update exercice.5.py

--- PHYTON/exercice.5.py
+++ PHYTON/exercice.5.py
@@ -1,16 +1,5 @@
-
-
 def count_in_list(lst, item):
-    """
-    Count the occurrences of an item in a list.
-
-    Args:
-        lst (list): The list to search.
-        item: The item to count.
-
-    Returns:
-        int: The number of occurrences of the item in the list.
-    """
+   
     return lst.count(item)
 
 
@@ -30,4 +19,5 @@ print("Testing tqdm:")
 for elem in tqdm(range(333)):
     sleep(0.005)
 print()
+
 
