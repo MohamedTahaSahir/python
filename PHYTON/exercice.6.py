5a0d4bdebc868e9e985076964c8c72b341dd921c
MohamedTahaSahir <159806448+MohamedTahaSahir@users.noreply.github.com>
2024-02-14 13:20:24 +0100

    Update exercice.6.py

--- PHYTON/exercice.6.py
+++ PHYTON/exercice.6.py
@@ -1,19 +1,1 @@
-# whatis.py
 
-import sys
-
-def check_number(num):
-    try:
-        num = int(num)
-        if num % 2 == 0:
-            print("I'm Even.")
-        else:
-            print("I'm Odd.")
-    except ValueError:
-        raise AssertionError("Argument is not an integer")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        raise AssertionError("More than one argument is provided")
-    else:
-        check_number(sys.argv[1])
